3b55523856bd0d2d559669ad0606264c4a42790d
logbird <logbird@126.com>
2013-02-02 00:24:58 +0800

    added php-cgi

--- server.c
+++ server.c
@@ -9,15 +9,19 @@
 #include <fcntl.h>
 
 #define MAX_BUFFER 1024 
+#define METHOD_SIZE 4 
 #define SERV_PORT 8080
 #define MAX_CONN 5
 #define space(x) isspace((int)x)
 
 
-void headers(int client);
 void requestHandle(int reqFd);
 int get_row_header(int fd, char *buf, size_t buf_size);
-void php_cgi(char const *path);
+void php_cgi(char const *path, int fd);
+int sendHeader(int fd, char const *header);
+void headers(int client);
+//read to space
+int read_to_space(char *ret, char const *buffer, int size);
 
 int main(int argc, char *argv[])
 {
@@ -42,10 +46,17 @@ int main(int argc, char *argv[])
     while(1)
     {
         reqFd = accept(servFd, (struct sockaddr*)NULL, NULL);
-        printf("accept\n");
         if(reqFd >= 0)
         {
-            requestHandle(reqFd);
+            pid = fork();
+            if(pid == 0)
+            {
+                requestHandle(reqFd);
+                exit(EXIT_SUCCESS);
+            }else if(pid > 0)
+            {
+                wait();
+            }
         }
         close(reqFd);
     }
@@ -55,26 +66,49 @@ int main(int argc, char *argv[])
 
 void requestHandle(int reqFd)
 {
-    char buffer[MAX_BUFFER - 1];
+    char buffer[MAX_BUFFER];
+    char method[METHOD_SIZE + 1];
+    char header[MAX_BUFFER];
+    char script[MAX_BUFFER];
+    int len = 0, tmp_len = 0, i = 0;
     //int flags;
-    int len = 0;
-
     //设置成非阻塞模式
     //flags = fcntl(reqFd, F_GETFL, 0);
     //fcntl(reqFd, F_SETFL, flags | O_NONBLOCK);
 
+    //First Row
+    len = get_row_header(reqFd, buffer, MAX_BUFFER);
+    //Get METHOD
+    read_to_space(method, buffer, METHOD_SIZE);
+    //Get Script
+    tmp_len = read_to_space(script, buffer, len);
+    snprintf(script, tmp_len + 1, ".%s", script);
+    fflush(stdout);
+    //!!!!!!!!!!!!!!!!!!!!!!!!!---需要更改buffer数据否则永远获取的是GET---!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
     headers(reqFd);
-    while(get_row_header(reqFd, buffer, MAX_BUFFER))
-    {
-        printf("%s", buffer);
-    }
+    php_cgi(script, reqFd);
 
     fflush(stdout);
     close(reqFd);
 }
 
-void php_cgi(char const *path)
+void php_cgi(char const *path, int fd)
 {
+    char script[500];
+    dup2(fd, STDOUT_FILENO);
+    dup2(fd, STDERR_FILENO);
+    strcpy(script, "SCRIPT_FILENAME=");
+    strcat(script, path);
+    putenv("GATEWAY_INTERFACE=CGI/1.1");
+    putenv(script);
+    putenv("QUERY_STRING=");
+    putenv("REQUEST_METHOD=GET");
+    putenv("REDIRECT_STATUS=true");
+    putenv("SERVER_PROTOCOL=HTTP/1.1");
+    putenv("REMOTE_HOST=127.0.0.1");
+    execl("/usr/local/php-dev/bin/php-cgi", "php-cgi", NULL);
+
 }
 
 //get a line HTTP header, return 0 if reaches the end.
@@ -100,13 +134,31 @@ int get_row_header(int fd, char *buf, size_t buf_size)
 void headers(int client)
 {
     char buf[MAX_BUFFER];
-
-    strcpy(buf, "HTTP/1.1 200 OK\r\n");
-    send(client, buf, strlen(buf), 0);
-    sprintf(buf, "Content-Type: text/html\r\n");
-    send(client, buf, strlen(buf), 0);
-    strcpy(buf, "\r\n");
-    send(client, buf, strlen(buf), 0);
-    strcpy(buf, "<html><head></head><body>Hello World!</body></html>");
-    send(client, buf, strlen(buf), 0);
+    sendHeader(client, "HTTP/1.1 200 OK\r\n");
+    sendHeader(client, "Content-Type: text/html\r\n");
 }
+
+int sendHeader(int fd, char const *header)
+{
+    char buf[MAX_BUFFER];
+    if(buf == NULL)
+    {
+        return -1;
+    }
+    sprintf(buf, "%s", header);
+    send(fd, buf, strlen(buf), 0);
+    return 1;
+}
+
+int read_to_space(char *ret, char const *buffer, int size)
+{
+    int i = 0;
+    while(i < size && !space(buffer[i]))
+    {
+        ret[i] = buffer[i];
+        i++;
+    }
+    ret[i] = '\0';
+    return i;
+}
+
